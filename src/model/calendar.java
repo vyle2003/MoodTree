344fdc8be67f615aee18581ca3b11556e7c89b41
Bryan-CS-Liao <140285717+Bryan-CS-Liao@users.noreply.github.com>
2023-10-21 11:40:04 -0700

    testing date function

---
+++ src/model/calendar.java
@@ -0,0 +1,17 @@
+package model;
+import java.util.Date;
+
+
+public class calender {
+
+    public static void main(String[] args) {
+    
+        Date d = new Date(0, 0, 0);
+        System.out.println( d.getTime() );
+
+        
+
+
+
+    }
+}
