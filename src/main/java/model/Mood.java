6dc0522e440b5b645e38677b225203384c6e14a5
alice <vyle2003@students.cs.ubc.ca address>
2023-10-21 13:08:01 -0700

    add mood class

---
+++ src/main/java/model/Mood.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Mood {
+    private String elated;
+    private String happy;
+    private String soso;
+    private String bored;
+    private String sad;
+
+    public Mood() {
+        elated = "#217a34";
+        happy = "#8aa62d";
+        soso= "#ccc90e";
+        bored = "#c77904";
+        sad = "#6b4713";
+    }
+}
