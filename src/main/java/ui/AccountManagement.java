e125a171d8188a0f870b2b83699bb4d7d1587528
SYFIRRY-LAPTOP\Syfirry <syfirry@gmail.com>
2023-10-21 16:55:34 -0700

    DatabaseConnection added Sign up added

--- src/main/java/ui/AccountManagement.java
+++ src/main/java/ui/AccountManagement.java
@@ -1,20 +1,34 @@
 package ui;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 @Controller
-public class AccountManagement{
+public class AccountManagement extends Application{
 
     @RequestMapping("/index")
-    public String processForm(@RequestParam("page") String page, @RequestParam("email") String email, @RequestParam("password") String password) {
-        if ("login".equals(page)) {
-            System.out.println();
-        } else if ("signup".equals(page)) {
-            System.out.println();
+    public String loginForm(@RequestParam("email") String email, @RequestParam("password") String password) {
+        return null;
+    }
+
+    @RequestMapping("/signup")
+    public String signUpForm(@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password) {
+        String insertSQL = "INSERT INTO users (username, password) VALUES (?, ?)";
+        try {
+            Connection dc = new DatabaseConnection().connectDB();
+            PreparedStatement preparedStatement = dc.prepareStatement(insertSQL);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return page;
+        return "main";
     }
 }
---
+++ src/main/java/ui/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package ui;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public Connection connectDB() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:./UserData.db";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conn;
+    }
+}
