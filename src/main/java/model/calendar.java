f6ecac9ebd9f0207af3daf274ba2f8a27a4d7733
Bryan-CS-Liao <140285717+Bryan-CS-Liao@users.noreply.github.com>
2023-10-21 12:12:24 -0700

    getters and setters for information in calendar

--- src/main/java/model/calendar.java
+++ src/main/java/model/calendar.java
@@ -1,20 +1,46 @@
 package main.java.model;
+import java.time.LocalDate;
 import java.util.Date;
-import java.util.Calendar;
-
+import java.util.List;
 
 public class calendar {
+    private String title;
+    LocalDate date = LocalDate.now();
+    private String mood;
 
-    public static void main(String[] args) {
-    
-        Calendar c = Calendar.getInstance();
-        System.out.println(c.getTime() );
-
-        
-
-        
-
-
-
+    // Get mood
+    public String getMood() {
+        return(mood);
     }
+
+    // Get title
+    public String getTitle() {
+        return(title);
+    }
+
+    //Get date
+    public int getDate() {
+        return(date.getDayOfMonth());
+    }
+
+    //Get month
+    public int getMonth() {
+        return(date.getMonthValue());
+    }
+
+    //Get year
+    public int getYear() {
+        return(date.getYear());
+    }
+
+    //Set mood
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+        
+
+    
+        
+
 }
