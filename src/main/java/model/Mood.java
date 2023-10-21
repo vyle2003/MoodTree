190e2cedbda640ce4896e0df42a3f85133dfdcfe
alice <vyle2003@students.cs.ubc.ca address>
2023-10-21 14:03:08 -0700

    add mood class and change final values

--- src/main/java/model/Mood.java
+++ src/main/java/model/Mood.java
@@ -1,17 +1,25 @@
 package model;
 
 public class Mood {
-    private String elated;
-    private String happy;
-    private String soso;
-    private String bored;
-    private String sad;
+    public static final Mood ECSTATIC = new Mood("Ecstatic", "#217a34");
+    public static final Mood HAPPY = new Mood("Happy", "#8aa62d");
+    public static final Mood BORED = new Mood("Bored", "#c77904");
+    public static final Mood NEUTRAL = new Mood("Neutral", "#ccc90e");
+    public static final Mood SAD = new Mood("Sad", "#6b4713");
 
-    public Mood() {
-        elated = "#217a34";
-        happy = "#8aa62d";
-        soso= "#ccc90e";
-        bored = "#c77904";
-        sad = "#6b4713";
+    private String moodName;
+    private String hexColor;
+
+    public Mood(String moodName, String hexColor) {
+        this.moodName = moodName;
+        this.hexColor = hexColor;
+    }
+
+    public String getMoodName() {
+        return moodName;
+    }
+
+    public String getHexColor() {
+        return hexColor;
     }
 }
