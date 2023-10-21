ef3f89bb36cbc003a6b0ebfd682cdb70fedeb0b8
alice <vyle2003@students.cs.ubc.ca address>
2023-10-21 12:20:00 -0700

    Leaf update

--- src/main/java/model/Leaf.java
+++ src/main/java/model/Leaf.java
@@ -1,7 +1,52 @@
-package model;
+package main.java.model;
+
+import java.util.Scanner;
 
 public class Leaf {
-    public static void main(String[] args) {
+    private String mood;
+    private String description;
+ //   private String darkGreen = "#217a34";
+//    private String green = "#8aa62d";
+//    private String yellow = "#ccc90e";
+//    private String brownish = "#c77904";
+//    private String brown = "#6b4713";
+
+    //Initialize
+    public Leaf() {
+        mood = "Insert Mood ";
+        description = "No description provided";
+    }
+
+    //Constructor
+    public Leaf(String mood, String description) {
+        this.mood = mood;
+        this.description = description;
+    }
+
+    //GETTER
+    public String getMood() {
+        return this.mood;
+    }
+
+    //SETTER
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    //GETTER
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void inputFromUser() {
+        Scanner scanner = new Scanner(System.in);
+
 
     }
+
+
 }
