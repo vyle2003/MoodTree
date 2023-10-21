4740d04f2046adc0d2c274d8c375905508981c90
Bryan-CS-Liao <140285717+Bryan-CS-Liao@users.noreply.github.com>
2023-10-21 11:49:07 -0700

    fixed somethings

--- src/main/java/model/Leaf.java
+++ src/main/java/model/Leaf.java
@@ -1,4 +1,4 @@
-package model;
+package main.java.model;
 
 public class Leaf {
     public static void main(String[] args) {
--- src/main/java/ui/main.java
+++ src/main/java/ui/main.java
@@ -1,4 +1,4 @@
-package ui;
+package main.java.ui;
 
 public class main {
 }
